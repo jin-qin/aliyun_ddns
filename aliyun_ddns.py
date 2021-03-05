22ab00cb91fff9775a91f04f8a1942d00d874a1b
Jing Qin <qin_jin@live.com>
2021-03-05 06:15:35 -0800

    update: support for both ipv4 and ipv6

--- aliyun_ddns.py
+++ aliyun_ddns.py
@@ -12,9 +12,13 @@ import hmac
 import base64
 
 REQUEST_URL = 'https://alidns.aliyuncs.com/'
-LOCAL_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ip.txt')
+LOCAL_FILE_V4 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ipv4.txt')
+LOCAL_FILE_V6 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ipv6.txt')
 ALIYUN_SETTINGS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'aliyun_settings.json')
-DOMAIN_TYPE = 'AAAA'
+DOMAIN_TYPE_V4 = 'A'
+DOMAIN_TYPE_V6 = 'AAAA'
+V4 = 'v4'
+V6 = 'V6'
 
 def get_common_params(settings):
 	"""
@@ -52,7 +56,7 @@ def get_signed_params(http_method, params, settings):
 
 	return params
 
-def update_yun(ip):
+def update_yun(ip, ver):
 	"""
 	修改云解析
 	参考文档：
@@ -61,6 +65,11 @@ def update_yun(ip):
 	"""
 	with open(ALIYUN_SETTINGS, 'r') as f:
 		settings = json.loads(f.read())
+
+	if ver == V4:
+		DOMAIN_TYPE = DOMAIN_TYPE_V4
+	elif ver == V6:
+		DOMAIN_TYPE = DOMAIN_TYPE_V6
 
 	#首先获取解析列表
 	domain = settings['domain']
@@ -95,32 +104,47 @@ def update_yun(ip):
 		print(result)
 
 def get_curr_ip():
-	rsp = requests.get('https://api6.my-ip.io/ip')
-	return rsp.text
+	ipv4 = requests.get('https://api4.my-ip.io/ip').text
+	ipv6 = requests.get('https://api6.my-ip.io/ip').text
+	return ipv4, ipv6
 
 def get_lastest_local_ip():
 	"""
 	获取最近一次保存在本地的ip
 	"""
-	print('ip local path', LOCAL_FILE)
+	print('ip local path', LOCAL_FILE_V4, LOCAL_FILE_V6)
 
 	fmod = 'r'
-	if not os.path.exists(LOCAL_FILE):
+	if not os.path.exists(LOCAL_FILE_V4):
 		fmod='w+'
-	with open(LOCAL_FILE, fmod) as f:
-		last_ip = f.readline()
-	return last_ip
+	with open(LOCAL_FILE_V4, fmod) as f:
+		last_ipv4 = f.readline()
+
+	if not os.path.exists(LOCAL_FILE_V6):
+		fmod='w+'
+	with open(LOCAL_FILE_V6, fmod) as f:
+		last_ipv6 = f.readline()
+
+	return last_ipv4, last_ipv6
 
 if __name__ == '__main__':
-	ip = get_curr_ip()
-	if not ip:
+	ipv4, ipv6 = get_curr_ip()
+	if not ipv4 and not ipv6:
 		print('get ip failed')
 	else:
-		last_ip = get_lastest_local_ip()
-		print(ip, last_ip)
-		if ip != last_ip:
-			print('save ip to {}...'.format(LOCAL_FILE))
-			with open(LOCAL_FILE, 'w') as f:
-				f.write(ip)
-			print('update remote record...')
-			update_yun(ip)
+		last_ipv4, last_ipv6 = get_lastest_local_ip()
+		print(ipv4, last_ipv4)
+		print(ipv6, last_ipv6)
+		if ipv4 != last_ipv4:
+			print('save ip to {}...'.format(LOCAL_FILE_V4))
+			with open(LOCAL_FILE_V4, 'w') as f:
+				f.write(ipv4)
+			print('update ipv4 remote record...')
+			update_yun(ipv4, V4)
+		
+		if ipv6 != last_ipv6:
+			print('save ip to {}...'.format(LOCAL_FILE_V6))
+			with open(LOCAL_FILE_V6, 'w') as f:
+				f.write(ipv6)
+			print('update ipv6 remote record...')
+			update_yun(ipv6, V6)
